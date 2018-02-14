05b04d3925c7a10dbd68fc4a48ccda965edb2394
marisaslozano <marisalozano1995@gmail.com>
2018-02-14 07:19:02 -0800

    simple gui showing up in maya

---
+++ gui.py
@@ -0,0 +1,46 @@
+import os
+import logging
+from PySide import QtGui, QtCore, QtUiTools
+from shiboken import wrapInstance
+import pymel.core as pm
+import maya.OpenMayaUI as omui 
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
+
+
+def getMayaWindow():
+    pointer = omui.MQtUtil.mainWindow()
+    if pointer:
+        return wrapInstance(long(pointer), QtGui.QMainWindow)
+
+window = None
+
+
+def run():
+    global window
+    if window:
+        window.close()
+    window = ImageGrabberUI(parent=getMayaWindow())
+    window.show()
+
+
+class ImageGrabberUI(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(ImageGrabberUI, self).__init__(parent)
+
+        self.gridLayout = QtGui.QGridLayout()
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.screenGrabPushButton = QtGui.QPushButton("Take Screenshot")
+        self.verticalLayout.addWidget(self.screenGrabPushButton)
+        self.previousImagesLabel = QtGui.QLabel("Previous Screenshots:")
+        self.verticalLayout.addWidget(self.previousImagesLabel)
+        self.imagePathLabel = QtGui.QLabel("Path:")
+        self.verticalLayout.addWidget(self.imagePathLabel)
+        self.pathLineEdit = QtGui.QLineEdit()
+        self.verticalLayout.addWidget(self.pathLineEdit)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+        self.setLayout(self.gridLayout)
+        self.show()
