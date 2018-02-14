6cd8d3045e57fdb13c5ebf10250f23dd665dec4f
marisaslozano <marisalozano1995@gmail.com>
2018-02-14 07:58:50 -0800

    testing to see if I can add multiple pictures to a folder

--- screenGrab.py
+++ screenGrab.py
@@ -1,5 +1,25 @@
+import os
 import logging
-import pymel.core as pm
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
+
+imagesFolderPath = os.path.join(os.path.dirname(__file__), 'screenCaptures')
+
+if not os.path.exists(imagesFolderPath):
+    os.makedirs(imagesFolderPath)
+
+
+def getScreenGrab():
+    import maya.OpenMaya
+    import maya.OpenMayaUI
+
+    view = maya.OpenMayaUI.M3dView.active3dView()
+
+    image = maya.OpenMaya.MImage()
+
+    view.readColorBuffer(image, True)
+
+    imageFileName = os.path.join(imagesFolderPath, "{0}.png".format("test"))
+
+    image.writeToFile(imageFileName, 'png')
