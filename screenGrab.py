f04f58f767e57c9f35f5990647839aa856dd3b6d
marisaslozano <marisalozano1995@gmail.com>
2018-02-15 07:49:32 -0800

    add crpping function from other tool

--- screenGrab.py
+++ screenGrab.py
@@ -5,9 +5,26 @@ _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 
 imagesFolderPath = os.path.join(os.path.dirname(__file__), 'screenCaptures')
+imagesConvertPath = os.path.join(os.path.dirname(__file__), 'imagesConvert.py')
 
 if not os.path.exists(imagesFolderPath):
     os.makedirs(imagesFolderPath)
+
+
+def cropImage(count=None):
+    import subprocess
+
+    imageStart = os.path.join(imagesFolderPath, "model_{0}.png".format(count))
+    imageResult = os.path.join(
+        imagesFolderPath, "model_{0}.jpeg".format(count))
+
+    cmd = ['python', imagesConvertPath,
+           '-ip', imageStart,
+           '-irp', imageResult]
+
+    p = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    resolution, error = p.communicate()
 
 
 def getScreenGrab(count=None):
@@ -20,6 +37,7 @@ def getScreenGrab(count=None):
 
     view.readColorBuffer(image, True)
 
-    imageFileName = os.path.join(imagesFolderPath, "{0}.png".format(count))
+    imageFileName = os.path.join(
+        imagesFolderPath, "model_{0}.png".format(count))
 
     image.writeToFile(imageFileName, 'png')
